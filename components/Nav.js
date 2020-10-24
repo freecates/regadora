31bb18984b35868737f66f0e7b9d45104a7aa93d
Ramon Gil <ramon.gil@deporvillage.com>
2020-10-24 19:36:27 +0200

    main new colors and sass modules

--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,9 @@
 import Link from 'next/link';
+import styles from './Nav.module.scss';
 
 const Nav = ({ title }) => {
     return (
-        <nav>
+        <nav className={styles.nav}>
             <ul>
                 <li>
                     <Link href='/'>
