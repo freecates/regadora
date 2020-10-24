e7ee904eb3c52bf3096e951c9d64eeb8ebbcf661
Ramon Gil <ramon.gil@deporvillage.com>
2020-10-24 20:22:18 +0200

    main, added active navigation

--- components/Nav.js
+++ components/Nav.js
@@ -1,11 +1,14 @@
 import Link from 'next/link';
+import { useRouter } from "next/router";
 import styles from './Nav.module.scss';
 
 const Nav = ({ title }) => {
+
+    const router = useRouter();
     return (
         <nav className={styles.nav}>
             <ul>
-                <li>
+                <li className={router.pathname == "/" ? "active" : ""}>
                     <Link href='/'>
                         <a>
                             <img
@@ -18,7 +21,7 @@ const Nav = ({ title }) => {
                         </a>
                     </Link>
                 </li>
-                <li>
+                <li className={`${router.pathname == '/qui-som' ? styles.active : ''}`}>
                     <Link href='/qui-som'>Qui som</Link>
                 </li>
             </ul>
