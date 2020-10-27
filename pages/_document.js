167a96ba49316d39d10d389d3060f2a4302d0b6c
Ramon Gil <ramon.gil@deporvillage.com>
2020-10-27 17:48:59 +0100

    main, properly preconnect origins

--- pages/_document.js
+++ pages/_document.js
@@ -10,9 +10,11 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                    <link rel='preconnect' href='https://fonts.gstatic.com/' crossorigin />
+
                     <link
                         href='https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap'
-                        rel='preconnect'
+                        rel='stylesheet'
                     />
                 </Head>
                 <body>
