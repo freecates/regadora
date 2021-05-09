eafc9ffab7071316143adc57b51b43b9ffcea3e3
Ramon Gil <ramon.gil@deporvillage.com>
2021-05-09 17:23:18 +0200

    with-keen-slider , added slider to home

--- components/imagecarousel/index.js
+++ components/imagecarousel/index.js
@@ -1,11 +1,59 @@
-import Carousel from 'react-gallery-carousel';
-import 'react-gallery-carousel/dist/index.css';
+import React from 'react';
+import { useKeenSlider } from 'keen-slider/react';
+import 'keen-slider/keen-slider.min.css';
 import styles from './ImageCarousel.module.scss';
 
 const ImageCarousel = ({ images }) => {
+    const [pause, setPause] = React.useState(false);
+    const timer = React.useRef();
+    const [opacities, setOpacities] = React.useState([]);
+
+    const [sliderRef, slider] = useKeenSlider({
+        slides: images.length,
+        loop: true,
+        duration: 4000,
+        move(s) {
+            const new_opacities = s.details().positions.map((slide) => slide.portion);
+            setOpacities(new_opacities);
+        },
+        dragStart: () => {
+            setPause(true);
+        },
+        dragEnd: () => {
+            setPause(false);
+        },
+    });
+    React.useEffect(() => {
+        sliderRef.current.addEventListener('mouseover', () => {
+            setPause(true);
+        });
+        sliderRef.current.addEventListener('mouseout', () => {
+            setPause(false);
+        });
+    }, [sliderRef]);
+
+    React.useEffect(() => {
+        timer.current = setInterval(() => {
+            if (!pause && slider) {
+                slider.next();
+            }
+        }, 4000);
+        return () => {
+            clearInterval(timer.current);
+        };
+    }, [pause, slider]);
+
     return (
-        <div className={styles.wrapper}>
-            <Carousel images={images} hasMediaButton={false} hasThumbnails={false} isAutoPlaying={true} hasSizeButton={false} hasIndexBoard={false} style={{height: 'auto'}} />
+        <div ref={sliderRef} className={`${styles.wrapper} ${styles.fader}`}>
+            {images.map((i, idx) => (
+                <div
+                    key={idx}
+                    className={`${styles.fader__slide}`}
+                    style={{ opacity: opacities[idx] }}
+                >
+                    <img src={i.src} alt={i.alt} loading={'lazy'} height={1280} width={1920} />
+                </div>
+            ))}
         </div>
     );
 };
